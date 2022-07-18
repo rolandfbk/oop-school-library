f55c3b67390b24d80defea72e0959cee5712d358
rolandfbk <rolandfbk@gmail.com>
2022-07-18 15:36:58 +0200

    class person, class student, class teacher created

---
+++ person.rb
@@ -0,0 +1,39 @@
+class Person
+  def initialize(age, name = "Unknown", parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def id
+    @id
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+  def name(value)
+    @name = value
+  end
+
+  def age(value)
+    @age = value
+  end
+
+  def can_use_services?
+    (is_of_age? == true || parent_permission == true)? true : false
+  end
+
+  private
+
+  def is_of_age?
+    @age >= 18? false : true
+  end
+
+end
