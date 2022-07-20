c9fa281d41d26cccf2dc2465a6e27d1b6b99f0ad
rolandfbk <rolandfbk@gmail.com>
2022-07-20 18:48:21 +0200

    Associations setup done

--- person.rb
+++ person.rb
@@ -1,16 +1,18 @@
 require './nameable'
 
 class Person < Nameable
+  attr_reader :rentals, :id
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     super()
+    @rentals = []
   end
 
   attr_accessor :name, :age
-  attr_reader :id
 
   def correct_name
     @name
